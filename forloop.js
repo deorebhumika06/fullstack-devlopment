9ddcb9ba7971035bc0f2f45e00861468f3c38e56
deorebhumika06 <deorebhumika06@gmail.com>
2026-01-23 10:40:07 +0530

    Create forloop.js

---
+++ forloop.js
@@ -0,0 +1,20 @@
+for(let i=0;i<10;i++)
+{
+    if(i%2==10)
+    {
+        console.log("even no",+i)
+    }
+    else{
+        console.log("odd no",+i)
+    }
+    console.log(i)
+}
+//break
+for(let i=0;i<10;i++)
+{
+    if(i==5)
+    {
+        break;
+    }
+    console.log(i)
+}
